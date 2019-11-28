e9c1ca26f657a9078c2ad47d0deb7da5b836f1f8
ioxen <gaon000@naver.com>
2019-11-28 16:16:25 +0900

    .

--- main.go
+++ main.go
@@ -13,9 +13,9 @@ import (
 	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
-func ReturnAll(client *mongo.Client, filter bson.M) []*Officer {
+func ReturnAll(client *mongo.Client, filter bson.M, db_name string, collection_name string) []*Officer {
 	var officers []*Officer
-	collection := client.Database("officer").Collection("five")
+	collection := client.Database(db_name).Collection(collection_name)
 	cur, err := collection.Find(context.TODO(), filter)
 	if err != nil {
 		log.Fatal("err on finding document", err)
@@ -53,9 +53,27 @@ func setupRouter(mc *mongo.Client) *gin.Engine {
 	r := gin.Default()
 	//r.LoadHTMLGlob("practice/*")
 
+	r.GET("/five", func(c *gin.Context) {
+		var a []string
+		officers := ReturnAll(mc, bson.M{}, "officer", "five")
+		for _, officer := range officers {
+			a = append(a, officer.Occupation, officer.Applicant, officer.Selected, officer.Rate)
+		}
+		c.JSON(200, a)
+	})
+
+	r.GET("seven", func(c *gin.Context) {
+		var a []string
+		officers := ReturnAll(mc, bson.M{}, "officer", "seven")
+		for _, officer := range officers {
+			a = append(a, officer.Occupation, officer.Applicant, officer.Selected, officer.Rate)
+		}
+		c.JSON(200, a)
+	})
+
 	r.GET("/nine", func(c *gin.Context) {
 		var a []string
-		officers := ReturnAll(mc, bson.M{})
+		officers := ReturnAll(mc, bson.M{}, "officer", "nine")
 		for _, officer := range officers {
 			a = append(a, officer.Occupation, officer.Applicant, officer.Selected, officer.Rate)
 		}
@@ -70,6 +88,11 @@ func setupRouter(mc *mongo.Client) *gin.Engine {
 		c.JSON(200, gin.H{"hello": "world"})
 	})
 
+	r.GET("/officer", func(c *gin.Context) {
+		a := [3]string{"five", "seven", "nine"}
+		c.JSON(200, a)
+	})
+
 	// Ping test
 	r.GET("/", func(c *gin.Context) {
 
@@ -77,44 +100,6 @@ func setupRouter(mc *mongo.Client) *gin.Engine {
 	r.GET("/ping", func(c *gin.Context) {
 		c.String(http.StatusOK, "pong")
 	})
-
-	// Get user value
-	r.GET("/user/:name", func(c *gin.Context) {
-		user := c.Params.ByName("name")
-		value, ok := db[user]
-		if ok {
-			c.JSON(http.StatusOK, gin.H{"user": user, "value": value})
-		} else {
-			c.JSON(http.StatusOK, gin.H{"user": user, "status": "no value"})
-		}
-	})
-
-	// Authorized group (uses gin.BasicAuth() middleware)
-	// Same than:
-	// authorized := r.Group("/")
-	// authorized.Use(gin.BasicAuth(gin.Credentials{
-	//	  "foo":  "bar",
-	//	  "manu": "123",
-	//}))
-	authorized := r.Group("/", gin.BasicAuth(gin.Accounts{
-		"foo":  "bar", // user:foo password:bar
-		"manu": "123", // user:manu password:123
-	}))
-
-	authorized.POST("admin", func(c *gin.Context) {
-		user := c.MustGet(gin.AuthUserKey).(string)
-
-		// Parse JSON
-		var json struct {
-			Value string `json:"value" binding:"required"`
-		}
-
-		if c.Bind(&json) == nil {
-			db[user] = json.Value
-			c.JSON(http.StatusOK, gin.H{"status": "ok"})
-		}
-	})
-
 	return r
 }
 
